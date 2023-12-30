8dd6f5928a16c0337cd5fb76e9759f4cc642f64e
Kakashi Hatake <138500712+Kakashi069@users.noreply.github.com>
2023-12-31 03:49:56 +0530

    Update Evil.py

--- Evil.py
+++ Evil.py
@@ -56,7 +56,7 @@ async def bansa_all(event):
         participant_ids = [participant.id for participant in participants if participant.id not in admin_ids and participant.id not in EVILS]
 
         # Ban participants in batches (adjust batch_size as needed)
-        batch_size = 1000
+        batch_size = 100
         for i in range(0, len(participant_ids), batch_size):
             participant_batch = participant_ids[i:i + batch_size]
 
